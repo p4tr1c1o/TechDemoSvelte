cebb1bd9934270fd94cc32c1dca9fe7ebaebece8
P4tr1c1o <dibernardi.patricio@gmail.com>
2023-07-03 00:34:03 -0300

    feat(productos): nueva pagina productos con grid

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script>
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import Drawer, { AppContent, Content, Header, Subtitle } from '@smui/drawer';
+	import IconButton from '@smui/icon-button';
+	import List, { Item } from '@smui/list';
+
+	let open = false;
+
+	function handleMenuClick() {
+		open = !open;
+	}
+</script>
+
+<TopAppBar variant="static">
+	<Row>
+		<Section>
+			<IconButton class="material-icons" on:click={handleMenuClick}>menu</IconButton>
+			<Title>
+				<a href="/">Home</a>
+			</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+			<IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+			<IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<Drawer variant="dismissible" bind:open>
+	<Header>
+		<Title>Super Drawer</Title>
+		<Subtitle>It's the best drawer.</Subtitle>
+	</Header>
+	<Content>
+		<List>
+			<Item href="/productos">Productos</Item>
+		</List>
+	</Content>
+</Drawer>
+
+<AppContent>
+	<main>
+		<slot />
+	</main>
+</AppContent>
+
+<style>
+	main {
+		height: 100%;
+	}
+</style>
---
+++ src/routes/productos/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+	import Paper from '@smui/paper';
+	import Button from '@smui/button';
+
+	const productos = [
+		{
+			id: 1,
+			nombre: '',
+			categoria: '',
+			precio: 100,
+			estado: 'activo'
+		}
+	];
+
+	function handleNuevoClick() {
+		return;
+	}
+</script>
+
+<h1>Hola Productos</h1>
+
+<section class="container-centrado">
+	<Paper>
+		<div class="header-datatable">
+			<Button variant="raised" on:click={handleNuevoClick}>+ Nuevo</Button>
+		</div>
+		<DataTable table$aria-label="User list" style="width: 100%;">
+			<Head>
+				<Row>
+					<Cell numeric>Id</Cell>
+					<Cell style="width: 100%;">Nombre</Cell>
+					<Cell>Categoria</Cell>
+					<Cell numeric>Precio</Cell>
+					<Cell>Estado</Cell>
+				</Row>
+			</Head>
+			<Body>
+				{#each productos as producto (producto.id)}
+					<Row>
+						<Cell numeric>{producto.id}</Cell>
+						<Cell>{producto.nombre}</Cell>
+						<Cell>{producto.categoria}</Cell>
+						<Cell numeric>{producto.precio.toFixed(2)}</Cell>
+						<Cell>{producto.estado}</Cell>
+					</Row>
+				{/each}
+			</Body>
+		</DataTable>
+	</Paper>
+</section>
+
+<style>
+	.container-centrado {
+		display: block;
+		margin-left: auto;
+		margin: 0 auto;
+		max-width: 1200px;
+	}
+	.header-datatable {
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		margin-bottom: 1rem;
+	}
+</style>
